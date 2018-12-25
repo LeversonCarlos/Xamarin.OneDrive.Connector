522a10a682522c2f47660b42392f8826f05b416b
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-25 12:35:16 -0200

    identity client

--- srcs/Token/Token.cs
+++ srcs/Token/Token.cs
@@ -1,18 +1,24 @@
 using System;
+using Microsoft.Identity.Client;
 
 namespace Xamarin.OneDrive
 {
    public partial class Token : IDisposable
    {
       public Configs Configs { get; private set; }
+      PublicClientApplication Client { get; set; }
 
       public Token(Configs configs)
       {
          this.Configs = configs;
+         this.Client = new PublicClientApplication(configs.ClientID);
+         if (!string.IsNullOrEmpty(configs.RedirectUri))
+         { this.Client.RedirectUri = configs.RedirectUri; }
       }
 
       public void Dispose()
       {
+         this.Client = null;
          this.Configs.Dispose();
          this.Configs = null;
       }
