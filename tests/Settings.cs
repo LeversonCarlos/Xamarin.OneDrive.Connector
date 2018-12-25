d891e41e2a3f724e9aa5d7c5065fff9ea1b9b0d5
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-25 12:31:26 -0200

    preparing tests execution

---
+++ tests/Settings.cs
@@ -0,0 +1,7 @@
+namespace Xamarin.OneDrive.Connector.Tests
+{
+   internal class Settings
+   {
+      public static string ClientID { get { return "MyClientID"; } }
+   }
+}
--- tests/TokenTests.cs
+++ tests/TokenTests.cs
@@ -6,16 +6,15 @@ namespace Xamarin.OneDrive.Connector.Tests
 {
    public class TokenTests
    {
-      const string TestClientID = "MyClientID";
 
       [Fact]
       public void ClientProperty_MustMatch_Definition()
       {
-         using (var configs = new Configs { ClientID = TestClientID })
+         using (var configs = new Configs { ClientID = Settings.ClientID })
          {
             using (var token = new Token(configs))
             {
-               Assert.Equal(TestClientID, token.Configs.ClientID);
+               Assert.Equal(Settings.ClientID, token.Configs.ClientID);
             }
          }
       }
