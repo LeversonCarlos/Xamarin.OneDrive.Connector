486918d670d91ed4b120d6388f1b642719c5c659
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-26 09:17:04 -0200

    refresh token

---
+++ srcs/Token/Token.Refresh.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamarin.OneDrive
+{
+   partial class Token
+   {
+
+      public async Task<bool> RefreshAsync()
+      {
+         try
+         {
+            var accounts = await this.Client.GetAccountsAsync();
+            if (accounts != null && accounts.Count() != 0)
+            {
+               var account = accounts.FirstOrDefault();
+               if (account != null)
+               {
+                  this.AuthResult = await this.Client.AcquireTokenSilentAsync(this.Configs.Scopes, account);
+               }
+            }
+            return this.IsValid();
+         }
+         catch (Exception) { return false; }
+      }
+
+   }
+}
