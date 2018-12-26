356c3f3cc7af3572c6027853783ace37fca5584b
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-26 11:49:47 -0200

    netStandard specific implementation

---
+++ srcs/Configs/Dependency.shared.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+
+namespace Xamarin.OneDrive
+{
+
+   internal interface IDependency
+   {
+      void Initialize(Configs configs);
+   }
+
+   internal class Dependency
+   {
+
+      static Lazy<IDependency> implementation = new Lazy<IDependency>(() => CreateDependency(), LazyThreadSafetyMode.PublicationOnly);
+
+      static IDependency CreateDependency()
+      {
+         // #pragma warning disable IDE0022 // Use expression body for methods
+         return new DependencyImplementation();
+         // #pragma warning restore IDE0022 // Use expression body for methods
+      }
+
+      public static IDependency Current
+      {
+         get
+         {
+            IDependency ret = implementation.Value;
+            if (ret == null)
+            {
+               throw new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+            }
+            return ret;
+         }
+      }
+
+   }
+
+}
