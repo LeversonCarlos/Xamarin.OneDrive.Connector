e7a6f4eeb193d666d5f870ce564ed5e5b7f648d9
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-26 09:24:13 -0200

    token connection

---
+++ srcs/Token/Token.Connection.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamarin.OneDrive
+{
+   partial class Token
+   {
+
+      public async Task<bool> ConnectAsync()
+      {
+         try
+         {
+
+            // TOKEN STILL VALID
+            if (this.IsValid()) { return true; }
+
+            // REFRESH AN EXPIRED TOKEN 
+            if (await this.RefreshAsync()) { return true; }
+
+            // ACQUIRE A NEW TOKEN 
+            if (await this.AcquireAsync()) { return true; }
+
+            // OTHERWISE
+            return false;
+
+         }
+         catch (Exception) { throw; }
+      }
+
+      public async Task DisconnectAsync()
+      {
+         try
+         {
+            var accounts = await this.Client.GetAccountsAsync();
+            if (accounts != null && accounts.Count() != 0)
+            {
+               foreach (var account in accounts)
+               { await this.Client.RemoveAsync(account); }
+            }
+
+         }
+         catch (Exception) { throw; }
+      }
+
+   }
+}
