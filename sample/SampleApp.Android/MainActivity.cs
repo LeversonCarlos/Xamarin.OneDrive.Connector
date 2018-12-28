cc60e46da5ea92c3a3b5ac15b383384c1f096ba8
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-28 15:16:09 -0200

    adjusting android sample

--- sample/SampleApp.Android/MainActivity.cs
+++ sample/SampleApp.Android/MainActivity.cs
@@ -1,25 +1,29 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace SampleApp.Droid
 {
-    [Activity(Label = "SampleApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+   [Activity(Label = "SampleApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+   {
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-    }
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         TabLayoutResource = Resource.Layout.Tabbar;
+         ToolbarResource = Resource.Layout.Toolbar;
+
+         base.OnCreate(savedInstanceState);
+         global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+         LoadApplication(new App());
+      }
+
+      protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+      {
+         base.OnActivityResult(requestCode, resultCode, data);
+         Xamarin.OneDrive.Configs.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+      }
+
+   }
 }
