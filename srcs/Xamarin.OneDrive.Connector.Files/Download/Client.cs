ab3f75426dbc3b7e53057c309ab5cc1ac869d88f
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-28 12:13:15 -0200

    files download url

---
+++ srcs/Xamarin.OneDrive.Connector.Files/Download/Client.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Xamarin.OneDrive.Files
+{
+   public static class DownloadConnector
+   {
+
+      public static async Task<string> GetDownloadUrlAsync(this Xamarin.OneDrive.Connector connector, FileData file)
+      {
+         try
+         { 
+
+            var httpPath = $"me/drive/items/{file.id}?select=id,@microsoft.graph.downloadUrl";
+            var httpMessage = await connector.GetAsync(httpPath);
+            if (!httpMessage.IsSuccessStatusCode)
+            { throw new Exception(await httpMessage.Content.ReadAsStringAsync()); }
+
+            var httpContent = await httpMessage.Content.ReadAsStreamAsync();
+            var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(FileData));
+            var httpResult = (FileData)serializer.ReadObject(httpContent);
+            return httpResult.downloadUrl;
+
+         }
+         catch (Exception) { throw; }
+      }
+
+   }
+}
