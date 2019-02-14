35b59815301a447dd8f1fa916680e8180b9a659a
Leverson Carlos <lcjohnny@hotmail.com>
2019-02-14 20:05:20 -0200

    GetDetailsAsync

--- srcs/Xamarin.OneDrive.Connector.Files/Files/Client.cs
+++ srcs/Xamarin.OneDrive.Connector.Files/Files/Client.cs
@@ -6,6 +6,47 @@ namespace Xamarin.OneDrive.Files
    public static partial class Extender
    {
 
+      public static async Task<FileData> GetDetailsAsync(this Xamarin.OneDrive.Connector connector)
+      {
+         var httpPath = $"me/drive/root";
+         return await GetDetailsAsync(connector, httpPath);
+      }
+
+      public static async Task<FileData> GetDetailsAsync(this Xamarin.OneDrive.Connector connector, FileData folder)
+      {
+         var httpPath = $"me/drive/items/{folder.id}";
+         return await GetDetailsAsync(connector, httpPath);
+      }
+
+      private static async Task<FileData> GetDetailsAsync(this Xamarin.OneDrive.Connector connector, string httpPath)
+      {
+         try
+         {
+            httpPath += "?select=id,name,parentReference&$top=1";
+
+            // REQUEST DATA FROM SERVER
+            var httpMessage = await connector.GetAsync(httpPath);
+            if (!httpMessage.IsSuccessStatusCode)
+            { throw new Exception(await httpMessage.Content.ReadAsStringAsync()); }
+
+            // SERIALIZE AND STORE RESULT
+            var httpContent = await httpMessage.Content.ReadAsStreamAsync();
+            var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(FileData));
+            var httpResult = (FileData)serializer.ReadObject(httpContent);
+
+            // RESULT
+            if (string.IsNullOrEmpty(httpResult.FilePath))
+            { httpResult.FilePath = httpResult.FileName; }
+            if (httpResult.parentReference != null)
+            {
+                httpResult.FilePath = httpResult.parentReference.FilePath; 
+            }
+            return httpResult;            
+
+         }
+         catch (Exception) { throw; }
+      }
+
 
    }
 }
