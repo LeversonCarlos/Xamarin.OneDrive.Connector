fbdf3f81135781ff2f7315891b4b64b01c882903
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-07 16:19:47 -0300

    bugFix

--- srcs/Xamarin.OneDrive.Connector/Configs/Configs.android.cs
+++ srcs/Xamarin.OneDrive.Connector/Configs/Configs.android.cs
@@ -7,6 +7,15 @@ namespace Xamarin.OneDrive
    partial class Connector
    {
 
+      /* 
+      internal Context _activity;
+      internal Bundle _bundle;
+      public static void Init(Context activity, Bundle bundle) {
+         this._activity=activity;
+         this._bundle=bundle;
+      }
+      */
+
       public static void SetAuthenticationContinuationEventArgs(int requestCode, Result resultCode, Intent data)
       {
          AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
