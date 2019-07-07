76594f542983474b754a27cabcb5284d102c3853
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-07 18:37:22 -0300

    init connection passing activitiy and redirection

--- srcs/Xamarin.OneDrive.Connector/Configs/Dependency.android.cs
+++ srcs/Xamarin.OneDrive.Connector/Configs/Dependency.android.cs
@@ -1,18 +1,32 @@
 using Android.App;
+using Android.Content;
+using Microsoft.Identity.Client;
 using System;
 using System.Threading.Tasks;
-using Microsoft.Identity.Client;
 
 namespace Xamarin.OneDrive
 {
+
    internal class DependencyImplementation : IDependency
    {
+      internal Context _activity;
+      internal string _redirectUrl;
 
       public void Initialize(Configs configs)
       {
-         var mainActivity = Xamarin.Forms.Forms.Context as Forms.Platform.Android.FormsAppCompatActivity;
-         configs.UiParent = mainActivity;
-         configs.RedirectUri = $"msal{configs.ClientID}://auth";
+         if (_activity != null) {
+            configs.UiParent = _activity;
+         }
+         else {
+            var mainActivity = Xamarin.Forms.Forms.Context as Forms.Platform.Android.FormsAppCompatActivity;
+            configs.UiParent = mainActivity;
+         }
+         if (!string.IsNullOrEmpty(_redirectUrl)) {
+            configs.RedirectUri = _redirectUrl;
+         }
+         else {
+            configs.RedirectUri = $"msal{configs.ClientID}://auth";
+         }
       }
 
       public async Task<AuthenticationResult> GetAuthResult(IPublicClientApplication client, Configs configs)
@@ -28,4 +42,18 @@ namespace Xamarin.OneDrive
       }
 
    }
+
+   partial class Connector
+   {
+
+      public static void Init(Context activity) { Init(activity, "");  }
+
+      public static void Init(Context activity, string redirectUrl) {
+         var dependency= (DependencyImplementation)Dependency.Current;
+         dependency._activity = activity;
+         dependency._redirectUrl = redirectUrl;
+      }
+
+   }
+
 }
