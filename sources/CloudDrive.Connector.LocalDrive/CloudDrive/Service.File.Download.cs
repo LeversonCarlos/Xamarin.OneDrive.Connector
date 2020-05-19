1e196da1f59daacb1134a7e9fc9c198e44bb1fd2
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-19 20:51:33 -0300

    splikting files

---
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.File.Download.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+namespace Xamarin.CloudDrive.Connector.LocalDrive
+{
+   partial class LocalDriveService
+   {
+
+      public Task<byte[]> Download(string fileID) => throw new System.NotImplementedException();
+
+   }
+}
--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
@@ -1,14 +1,9 @@
-using System.Threading.Tasks;
 using Xamarin.CloudDrive.Connector.Common;
 
 namespace Xamarin.CloudDrive.Connector.LocalDrive
 {
    public partial class LocalDriveService : ICloudDriveService
    {
-
-      public Task<FileVM> Upload(string fileID, byte[] fileContent) => throw new System.NotImplementedException();
-      public Task<FileVM> Upload(string directoryID, string fileName, byte[] fileContent) => throw new System.NotImplementedException();
-      public Task<byte[]> Download(string fileID) => throw new System.NotImplementedException();
 
    }
 }
