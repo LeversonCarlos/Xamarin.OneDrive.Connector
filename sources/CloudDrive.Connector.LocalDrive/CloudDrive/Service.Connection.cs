4f8f26e70ab32066f870cb447ae7d3a9d6de2477
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-19 20:44:50 -0300

    spliting files

---
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.Connection.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace Xamarin.CloudDrive.Connector.LocalDrive
+{
+   partial class LocalDriveService 
+   {
+
+      public Task<bool> Connect() => throw new System.NotImplementedException();
+      public Task Disconnect() => throw new System.NotImplementedException();
+      public Task<bool> IsConnected() => throw new System.NotImplementedException();
+
+   }
+}
--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
@@ -6,10 +6,6 @@ namespace Xamarin.CloudDrive.Connector.LocalDrive
    public partial class LocalDriveService : ICloudDriveService
    {
 
-      public Task<bool> Connect() => throw new System.NotImplementedException();
-      public Task Disconnect() => throw new System.NotImplementedException();
-      public Task<bool> IsConnected() => throw new System.NotImplementedException();
-
       public Task<ProfileVM> GetProfile() => throw new System.NotImplementedException();
 
       public Task<DirectoryVM[]> GetDrivers() => throw new System.NotImplementedException();
