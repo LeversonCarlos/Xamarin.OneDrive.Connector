fc1b6103139b20f9f11d0f7a952724632f47fd33
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-20 16:42:42 -0300

    upload implementation for android

---
+++ sources/CloudDrive.Connector.LocalDrive/Platforms/Android/Service.File.Upload.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.CloudDrive.Connector.Common;
+
+namespace Xamarin.CloudDrive.Connector.LocalDrive
+{
+   partial class LocalDriveService
+   {
+
+      public async Task<FileVM> Upload(string fileID, byte[] fileContent)
+      {
+         try
+         {
+            using (var fileStream = new FileStream(fileID, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+               await fileStream.WriteAsync(fileContent, 0, fileContent.Length);
+               await fileStream.FlushAsync();
+               fileStream.Close();
+            }
+            return await this.GetDetails(fileID);
+         }
+         catch (Exception) { throw; }
+      }
+
+      public Task<FileVM> Upload(string directoryID, string fileName, byte[] fileContent)
+      { return this.Upload($"{directoryID}{Path.DirectorySeparatorChar}{fileName}", fileContent); }
+
+   }
+}
