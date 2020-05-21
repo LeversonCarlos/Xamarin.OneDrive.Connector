3d08719e5ff88232483300eeff03f25bc9bfb05d
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-21 19:59:28 -0300

    constructor

--- sources/CloudDrive.Connector.OneDrive/Token/Token.cs
+++ sources/CloudDrive.Connector.OneDrive/Token/Token.cs
@@ -6,6 +6,13 @@ namespace Xamarin.CloudDrive.Connector.OneDrive
    public partial class Token : IToken
    {
 
+      public Token(params string[] scopes)
+      {
+         this.Scopes = scopes;
+      }
+
+      readonly string[] Scopes;
+
       public Task<bool> Connect() => throw new NotImplementedException();
       public Task<bool> IsConnected() => throw new NotImplementedException();
       public Task Disconnect() => throw new NotImplementedException();
