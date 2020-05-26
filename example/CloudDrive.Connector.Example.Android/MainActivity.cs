04c9458cc352dda6af6af81f8a8a49d3e83dec2b
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-26 20:23:47 -0300

    main activity

--- example/CloudDrive.Connector.Example.Android/MainActivity.cs
+++ example/CloudDrive.Connector.Example.Android/MainActivity.cs
@@ -1,7 +1,10 @@
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Xamarin.CloudDrive.Connector.LocalDrive;
+using Xamarin.CloudDrive.Connector.OneDrive;
 
 namespace Xamarin.CloudDrive.Connector.Example.Droid
 {
@@ -18,14 +21,22 @@ namespace Xamarin.CloudDrive.Connector.Example.Droid
 
          Xamarin.Essentials.Platform.Init(this, savedInstanceState);
          global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+         this.AddOneDriveConnector("{YOUR_MICROSOFT_APPLICATION_ID}", "User.Read", "Files.ReadWrite");
+         this.AddLocalDriveConnector(savedInstanceState);
          LoadApplication(new App());
       }
 
       public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
       {
          Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+         this.SetLocalDrivePermissionsResult(requestCode, permissions, grantResults);
          base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+      }
+
+      protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+      {
+         base.OnActivityResult(requestCode, resultCode, data);
+         this.SetOneDriveAuthenticationResult(requestCode, resultCode, data);
       }
 
    }
