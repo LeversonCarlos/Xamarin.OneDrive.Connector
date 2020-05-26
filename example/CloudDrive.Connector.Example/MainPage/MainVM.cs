75a52fffca8fe50f5a9fba0fc1a13f425df045b2
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-26 19:54:51 -0300

    connection

--- example/CloudDrive.Connector.Example/MainPage/MainVM.cs
+++ example/CloudDrive.Connector.Example/MainPage/MainVM.cs
@@ -1,29 +1,105 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.CloudDrive.Connector.Example.Helpers;
+using Xamarin.Forms;
 
 namespace Xamarin.CloudDrive.Connector.Example
 {
    public class MainVM : ObservableObject
    {
+      Common.ICloudDriveService DriveService;
 
       public MainVM()
       {
          this.Title = "CloudDrive Connector Example";
+         this.IsConnected = false;
+         this.ConnectionText = "Connect Account";
+         this.ConnectionColor = Color.Green;
+         this.ConnectionCommand = new Command(async () => await this.Connection());
       }
 
       string _SelectedCloundDrive;
       public string SelectedCloundDrive
       {
          get { return _SelectedCloundDrive; }
-         set { this.SetProperty(ref _SelectedCloundDrive, value); this.ServiceDisconnect(); }
+         set { this.SetProperty(ref _SelectedCloundDrive, value); this.Connection_Disconnect(); }
       }
       public List<string> CloundDriveList { get; set; } = new List<string> { "LocalDrive", "OneDrive" };
 
-      async Task ServiceDisconnect()
+      string _ConnectionText;
+      public string ConnectionText
       {
-         /* TODO */
+         get { return this._ConnectionText; }
+         set { this.SetProperty(ref this._ConnectionText, value); }
       }
+
+      Color _ConnectionColor;
+      public Color ConnectionColor
+      {
+         get { return this._ConnectionColor; }
+         set { this.SetProperty(ref this._ConnectionColor, value); }
+      }
+
+      bool _IsConnected;
+      public bool IsConnected
+      {
+         get { return this._IsConnected; }
+         set { this.SetProperty(ref this._IsConnected, value); }
+      }
+
+      public Command ConnectionCommand { get; set; }
+      async Task Connection()
+      {
+         try
+         {
+            this.IsBusy = true;
+            if (!this.IsConnected) { await this.Connection_Connect(); }
+            else { await this.Connection_Disconnect(); }
+         }
+         catch (Exception ex) { await this.DisplayAlert(ex.ToString()); }
+         finally { this.IsBusy = false; }
+      }
+
+      async Task Connection_Connect()
+      {
+
+         if (string.IsNullOrEmpty(this.SelectedCloundDrive)) { await this.DisplayAlert("Select the drive implementation first"); return; }
+         switch (this.SelectedCloundDrive)
+         {
+            case "LocalDrive":
+               this.DriveService = Common.DependencyProvider.Get<LocalDrive.LocalDriveService>(); break;
+            case "OneDrive":
+               this.DriveService = Common.DependencyProvider.Get<OneDrive.OneDriveService>(); break;
+            default:
+               await this.DisplayAlert("Select the drive implementation first"); return;
+         }
+
+         if (!await this.DriveService.ConnectAsync()) { return; }
+         await this.Connection_Connected();
+      }
+
+      async Task Connection_Connected()
+      {
+         var profile = await this.DriveService.GetProfile();
+         if (profile == null) { return; }
+         // this.ProfilePicture.Source = ImageSource.FromStream(() => new MemoryStream(profile.ProfilePicture));
+         this.ConnectionText = $"Disconnect from {profile.Description}";
+         this.ConnectionColor = Color.Red;
+         this.IsConnected = true;
+      }
+
+      async Task Connection_Disconnect()
+      {
+         await this.DriveService.DisconnectAsync();
+         this.ConnectionText = "Connect Account";
+         this.ConnectionColor = Color.Green;
+         // this.CurrentItem = null;
+         // this.CurrentItemImage = null;
+         this.IsConnected = false;
+      }
+
+      Task DisplayAlert(string message) => (App.Current.MainPage).DisplayAlert(this.Title, message, "Ok");
 
    }
 }
