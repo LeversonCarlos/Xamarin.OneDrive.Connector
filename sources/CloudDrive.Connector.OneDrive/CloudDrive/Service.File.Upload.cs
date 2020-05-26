7d41ba4256ece6d0f53eca0776a1c9eab4ecd4a3
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-26 17:30:28 -0300

    file UploadContent

--- sources/CloudDrive.Connector.OneDrive/CloudDrive/Service.File.Upload.cs
+++ sources/CloudDrive.Connector.OneDrive/CloudDrive/Service.File.Upload.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Xamarin.CloudDrive.Connector.Common;
 
@@ -8,6 +9,24 @@ namespace Xamarin.CloudDrive.Connector.OneDrive
 
       public Task<FileVM> Upload(string fileID, byte[] fileContent) => throw new System.NotImplementedException();
       public Task<FileVM> Upload(string directoryID, string fileName, byte[] fileContent) => throw new System.NotImplementedException();
+
+      private async Task<FileVM> UploadContent(string httpPath, byte[] content)
+      {
+         try
+         {
+
+            var httpData = new System.Net.Http.ByteArrayContent(content);
+            var httpMessage = await this.Client.PutAsync(httpPath, httpData);
+            if (!httpMessage.IsSuccessStatusCode) throw new Exception(await httpMessage.Content.ReadAsStringAsync());
+
+            var httpContent = await httpMessage.Content.ReadAsStreamAsync();
+            var fileDTO = await System.Text.Json.JsonSerializer.DeserializeAsync<DTOs.File>(httpContent);
+            var fileVM = this.GetDetails(fileDTO);
+
+            return fileVM;
+         }
+         catch (Exception) { throw; }
+      }
 
    }
 }
