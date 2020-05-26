a7a09203b25dc457a076124bfc914de09c5f646a
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-26 17:35:36 -0300

    startup extention

--- sources/CloudDrive.Connector.OneDrive/Platforms/NetStandard/Startup.cs
+++ sources/CloudDrive.Connector.OneDrive/Platforms/NetStandard/Startup.cs
@@ -5,7 +5,6 @@ namespace Xamarin.CloudDrive.Connector.OneDrive
    partial class Startup
    {
 
-      /*
       public static IServiceCollection AddOneDriveConnector(this IServiceCollection serviceCollection,
          string clientID, string redirectUri, string clientSecret, params string[] scopes)
       {
@@ -15,10 +14,9 @@ namespace Xamarin.CloudDrive.Connector.OneDrive
                var identity = Token.CreateIdentity(clientID, redirectUri, clientSecret);
                var token = new Token(identity, scopes);
                var client = new Client(token);
-               // return new OneDriveService(client);
+               return new OneDriveService(client);
             });
       }
-      */
 
    }
 }
