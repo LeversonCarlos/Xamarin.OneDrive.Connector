bdb707aa88187b4fd0ea67382d44ee5531775837
Leverson Carlos <lcjohnny@hotmail.com>
2020-05-27 18:56:10 -0300

    Bugfix on onedrive http client implementation (#29)

--- sources/CloudDrive.Connector.OneDrive/Client/Client.cs
+++ sources/CloudDrive.Connector.OneDrive/Client/Client.cs
@@ -10,7 +10,7 @@ namespace Xamarin.CloudDrive.Connector.OneDrive
       const string BaseURL = "https://graph.microsoft.com/v1.0/";
       readonly IToken Token;
 
-      public Client(IToken token)
+      public Client(IToken token) : base(new ClientHandler(() => token))
       {
          if (token == null) throw new ArgumentException("The token argument for the http client must be set");
          this.Token = token;
