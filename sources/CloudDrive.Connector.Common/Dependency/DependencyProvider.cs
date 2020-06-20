3a026d2da9b3af0b82311bf5ccd1548a8d0ed8d7
Leverson Carlos <lcjohnny@hotmail.com>
2020-06-19 21:14:04 -0300

    using lay for dependeny

--- sources/CloudDrive.Connector.Common/Dependency/DependencyProvider.cs
+++ sources/CloudDrive.Connector.Common/Dependency/DependencyProvider.cs
@@ -1,30 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Xamarin.CloudDrive.Connector.Common
 {
    public class DependencyProvider
    {
 
-      static Dictionary<Type, object> CreateCollection = new Dictionary<Type, object>();
-      static Dictionary<Type, object> ServiceCollection = new Dictionary<Type, object>();
+      static Dictionary<Type, object> Implementations = new Dictionary<Type, object>();
 
       public static void Add<T>(Func<T> createInstance)
       {
-         if (!CreateCollection.ContainsKey(typeof(T))) CreateCollection.Add(typeof(T), createInstance);
+         try
+         {
+            var type = typeof(T);
+            if (Implementations.ContainsKey(type)) { return; }
+            Lazy<T> implementation = new Lazy<T>(() => createInstance(), LazyThreadSafetyMode.PublicationOnly);
+            Implementations.Add(type, implementation);
+         }
+         catch (Exception) { throw; }
       }
 
       public static T Get<T>()
       {
-         var type = typeof(T);
-         if (!ServiceCollection.ContainsKey(type))
+         try
          {
-            if (!CreateCollection.ContainsKey(type)) { throw new Exception($"Type {type.FullName} not defined on service provider"); }
-            var create = (Func<T>)CreateCollection[type];
-            ServiceCollection.Add(type, create());
+            var type = typeof(T);
+            var implementation = (Lazy<T>)Implementations[type];
+            return implementation.Value;
          }
-         if (!ServiceCollection.ContainsKey(type)) { throw new Exception($"Type {type.FullName} could not be created on service provider"); }
-         return (T)ServiceCollection[type];
+         catch (Exception) { throw; }
       }
 
    }
