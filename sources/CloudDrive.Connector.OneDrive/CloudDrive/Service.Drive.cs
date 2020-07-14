964c08a8245242dfaf63ce55a92302aff609419c
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-14 13:03:20 -0300

    retrieve the shared drives that the user has access

--- sources/CloudDrive.Connector.OneDrive/CloudDrive/Service.Drive.cs
+++ sources/CloudDrive.Connector.OneDrive/CloudDrive/Service.Drive.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Xamarin.CloudDrive.Connector
@@ -10,16 +12,50 @@ namespace Xamarin.CloudDrive.Connector
       {
          try
          {
+            var drives = new List<DirectoryVM>();
+
+            // USER's ROOT DRIVE
             var profile = await this.GetProfile();
-            var resultData = new DirectoryVM
+            var rootDrive = new DirectoryVM
             {
-               ID = "root",
+               ID = $"{profile.ID}!root",
                Name = profile.Description,
                Path = "/"
             };
-            return new DirectoryVM[] { resultData };
+            drives.Add(rootDrive);
+
+            // LOCATE SHARED DRIVES
+            var sharedDrives = await GetSharedDrivers();
+            if (sharedDrives != null && sharedDrives.Length != 0)
+               drives.AddRange(sharedDrives);
+
+            return drives.ToArray();
          }
          catch (Exception) { throw; }
+      }
+
+      async Task<DirectoryVM[]> GetSharedDrivers()
+      {
+         try
+         {
+
+            var messageContent = await this.Client.GetAsync<DTOs.SharedDriveSearch>("me/drive/sharedWithMe");
+            if (messageContent == null || messageContent.value == null) return null;
+
+            var sharedDrives = messageContent.value
+               .Where(v => !string.IsNullOrEmpty(v.remoteItem?.shared?.owner?.user?.id))
+               .Select(v => new DirectoryVM
+               {
+                  ID = v.remoteItem.id,
+                  Name = $"{v.remoteItem.name}",
+                  Path = "/"
+               })
+               .ToArray();
+
+            return sharedDrives;
+
+         }
+         catch (Exception) { return null; }
       }
 
    }
