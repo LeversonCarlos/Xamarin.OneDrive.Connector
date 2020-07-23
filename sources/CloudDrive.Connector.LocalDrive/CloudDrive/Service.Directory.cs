2e4ee0c309804bbb1d2c6ddbdfba114457c4d914
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-23 19:56:30 -0300

    moving the get directories method into this place

--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.Directory.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.Directory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,32 @@ namespace Xamarin.CloudDrive.Connector
 {
    partial class LocalDriveService
    {
+
+      public async Task<DirectoryVM[]> GetDirectories(DirectoryVM directory)
+      {
+         try
+         {
+            if (!await CheckConnectionAsync()) return null;
+
+            if (directory == null) return null;
+            var directoryList = await GetDirectoryList(directory.ID);
+
+            var resultList = directoryList
+               .Select(dir => GetDirectoryInfo(dir))
+               .Where(dirInfo => dirInfo != null)
+               .Select(dirInfo => new DirectoryVM
+               {
+                  ID = dirInfo.FullName,
+                  Name = dirInfo.Name,
+                  Path = dirInfo.FullName,
+                  ParentID = directory.ID
+               })
+               .ToArray();
+
+            return resultList;
+         }
+         catch (Exception) { throw; }
+      }
 
       internal DirectoryInfo GetDirectoryInfo(string path)
       {
