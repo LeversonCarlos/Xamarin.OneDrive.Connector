ee6fcad97e702d102323aee1c9ab9ecc5665bd34
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-22 21:37:47 -0300

    make internals visible to the testing asemblies

--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.cs
@@ -1,3 +1,7 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("CloudDrive.Connector.LocalDriveTests")]
 namespace Xamarin.CloudDrive.Connector
 {
    public partial class LocalDriveService : ICloudDriveService
