cfb03052926866eb5847f589f97c72688cc5696a
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-24 19:38:01 -0300

    move contrauctor to startup fiel

--- sources/CloudDrive.Connector.LocalDrive/Platforms/Android/Startup.cs
+++ sources/CloudDrive.Connector.LocalDrive/Platforms/Android/Startup.cs
@@ -3,8 +3,15 @@ using Android.OS;
 
 namespace Xamarin.CloudDrive.Connector
 {
+
    partial class LocalDriveService
    {
+
+      internal LocalDriveService()
+      {
+         _Connection = new LocalDriveConnection();
+         _Storage = new LocalDriveStorage();
+      }
 
       public static void Init(Activity activity, Bundle bundle)
       {
