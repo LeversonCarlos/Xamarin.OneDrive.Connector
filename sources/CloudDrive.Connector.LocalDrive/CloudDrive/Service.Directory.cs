2b7f803c84cbc81d6ccce5f13dd75ae9f3748928
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-24 18:53:08 -0300

    remove the try as we couldnt create a single test that throws a exception here

--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.Directory.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.Directory.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,37 +9,33 @@ namespace Xamarin.CloudDrive.Connector
 
       public async Task<DirectoryVM[]> GetDirectories(DirectoryVM directory)
       {
-         try
-         {
-            if (!await CheckConnectionAsync()) return null;
+         if (!await CheckConnectionAsync()) return null;
 
-            if (directory == null) return null;
-            if (string.IsNullOrEmpty(directory.ID)) return null;
-            if (!Directory.Exists(directory.ID)) return null;
+         if (directory == null) return null;
+         if (string.IsNullOrEmpty(directory.ID)) return null;
+         if (!Directory.Exists(directory.ID)) return null;
 
-            var directoryListQuery = Directory
-               .EnumerateDirectories(directory.ID)
-               .Where(dir => !string.IsNullOrEmpty(dir))
-               .AsQueryable();
-            var directoryList = await Task.FromResult(directoryListQuery.ToArray());
+         var directoryListQuery = Directory
+            .EnumerateDirectories(directory.ID)
+            .Where(dir => !string.IsNullOrEmpty(dir))
+            .AsQueryable();
+         var directoryList = await Task.FromResult(directoryListQuery.ToArray());
 
-            var resultList = directoryList
-               .Where(dir => Directory.Exists(dir))
-               .OrderBy(dir => dir)
-               .Select(dir => new DirectoryInfo(dir))
-               .Where(dirInfo => dirInfo != null)
-               .Select(dirInfo => new DirectoryVM
-               {
-                  ID = dirInfo.FullName,
-                  Name = dirInfo.Name,
-                  Path = dirInfo.FullName,
-                  ParentID = directory.ID
-               })
-               .ToArray();
+         var resultList = directoryList
+            .Where(dir => Directory.Exists(dir))
+            .OrderBy(dir => dir)
+            .Select(dir => new DirectoryInfo(dir))
+            .Where(dirInfo => dirInfo != null)
+            .Select(dirInfo => new DirectoryVM
+            {
+               ID = dirInfo.FullName,
+               Name = dirInfo.Name,
+               Path = dirInfo.FullName,
+               ParentID = directory.ID
+            })
+            .ToArray();
 
-            return resultList;
-         }
-         catch (Exception) { throw; }
+         return resultList;
       }
 
    }
