685de588d732e2f4e7ce092d6014ba2fed207a83
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-25 11:49:03 -0300

    remove try catch as we couldnt fore an exception for it

--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.File.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.File.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,62 +9,54 @@ namespace Xamarin.CloudDrive.Connector
 
       public async Task<FileVM[]> GetFiles(DirectoryVM directory)
       {
-         try
-         {
-            if (!await CheckConnectionAsync()) return null;
+         if (!await CheckConnectionAsync()) return null;
 
-            if (directory == null) return null;
-            if (string.IsNullOrEmpty(directory.ID)) return null;
-            if (!Directory.Exists(directory.ID)) return null;
+         if (directory == null) return null;
+         if (string.IsNullOrEmpty(directory.ID)) return null;
+         if (!Directory.Exists(directory.ID)) return null;
 
-            var searchPattern = "*.*";
-            var searchOption = SearchOption.TopDirectoryOnly;
+         var searchPattern = "*.*";
+         var searchOption = SearchOption.TopDirectoryOnly;
 
-            var fileListQuery = Directory
-               .EnumerateFiles(directory.ID, searchPattern, searchOption)
-               .Where(file => !string.IsNullOrEmpty(file))
-               .Where(file => File.Exists(file))
-               .OrderBy(file => file)
-               .AsQueryable();
-            var fileList = await Task.FromResult(fileListQuery.ToArray());
+         var fileListQuery = Directory
+            .EnumerateFiles(directory.ID, searchPattern, searchOption)
+            .Where(file => !string.IsNullOrEmpty(file))
+            .Where(file => File.Exists(file))
+            .OrderBy(file => file)
+            .AsQueryable();
+         var fileList = await Task.FromResult(fileListQuery.ToArray());
 
-            var fileTasks = fileList
-               .Select(file => GetDetails(file))
-               .ToArray();
-            var fileTasksResult = await Task.WhenAll(fileTasks);
-            var fileResult = fileTasksResult
-               .Where(fileVM => fileVM != null)
-               .ToArray();
+         var fileTasks = fileList
+            .Select(file => GetDetails(file))
+            .ToArray();
+         var fileTasksResult = await Task.WhenAll(fileTasks);
+         var fileResult = fileTasksResult
+            .Where(fileVM => fileVM != null)
+            .ToArray();
 
-            return fileResult;
-         }
-         catch (Exception) { throw; }
+         return fileResult;
       }
 
       public async Task<FileVM> GetDetails(string fileID)
       {
-         try
+         if (!await CheckConnectionAsync()) return null;
+
+         if (string.IsNullOrEmpty(fileID)) return null;
+         if (!File.Exists(fileID)) return null;
+
+         var fileInfo = await Task.FromResult(new FileInfo(fileID));
+
+         var fileData = new FileVM
          {
-            if (!await CheckConnectionAsync()) return null;
+            ID = fileInfo.FullName,
+            Name = fileInfo.Name,
+            CreatedDateTime = fileInfo.CreationTime,
+            SizeInBytes = fileInfo.Length,
+            Path = fileInfo.DirectoryName,
+            ParentID = fileInfo.DirectoryName
+         };
 
-            if (string.IsNullOrEmpty(fileID)) return null;
-            if (!File.Exists(fileID)) return null;
-
-            var fileInfo = await Task.FromResult(new FileInfo(fileID));
-
-            var fileData = new FileVM
-            {
-               ID = fileInfo.FullName,
-               Name = fileInfo.Name,
-               CreatedDateTime = fileInfo.CreationTime,
-               SizeInBytes = fileInfo.Length,
-               Path = fileInfo.DirectoryName,
-               ParentID = fileInfo.DirectoryName
-            };
-
-            return fileData;
-         }
-         catch (Exception) { throw; }
+         return fileData;
       }
 
    }
