e061a17fca99a0cacdf90068a61e835b107dc33f
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-26 11:25:45 -0300

    helper class to clone test file

--- tests/CloudDrive.Connector.LocalDriveTests/Helpers/FileSystem.cs
+++ tests/CloudDrive.Connector.LocalDriveTests/Helpers/FileSystem.cs
@@ -1,8 +1,10 @@
+using System;
 using System.IO;
 using System.Linq;
 
 namespace Xamarin.CloudDrive.Connector.LocalDriveTests.Helpers
 {
+
    internal class FileSystem
    {
 
@@ -18,4 +20,36 @@ namespace Xamarin.CloudDrive.Connector.LocalDriveTests.Helpers
          .FirstOrDefault();
 
    }
+
+   internal class SampleClone : IDisposable
+   {
+
+      public SampleClone()
+      {
+         var sampleFile = FileSystem.SampleFile;
+         FilePath = $"{sampleFile}.clone";
+         FileContent = File.ReadAllBytes(sampleFile);
+      }
+
+      public readonly string FilePath;
+      public readonly byte[] FileContent;
+
+      public void WriteFile() =>
+         File.WriteAllBytes(FilePath, FileContent);
+
+      public void RemoveFile()
+      {
+         try
+         {
+            if (File.Exists(FilePath))
+               File.Delete(FilePath);
+         }
+         catch { }
+      }
+
+      public void Dispose() =>
+         RemoveFile();
+
+   }
+
 }
