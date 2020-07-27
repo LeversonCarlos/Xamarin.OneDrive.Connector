cb6f316b35fef649dd0338270727024fdd90e96b
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-27 20:29:27 -0300

    remove the try catch as we could find a way to throw it

--- sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.File.Search.cs
+++ sources/CloudDrive.Connector.LocalDrive/CloudDrive/Service.File.Search.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,37 +12,33 @@ namespace Xamarin.CloudDrive.Connector
 
       public async Task<FileVM[]> SearchFiles(DirectoryVM directory, string searchPattern, int limit)
       {
-         try
-         {
-            if (!await ConnectAsync()) return null;
+         if (!await ConnectAsync()) return null;
 
-            if (directory == null) return null;
-            if (string.IsNullOrEmpty(directory.ID)) return null;
-            if (!Directory.Exists(directory.ID)) return null;
-            if (limit <= 0) return null;
+         if (directory == null) return null;
+         if (string.IsNullOrEmpty(directory.ID)) return null;
+         if (!Directory.Exists(directory.ID)) return null;
+         if (limit <= 0) return null;
 
-            var searchOption = SearchOption.AllDirectories;
+         var searchOption = SearchOption.AllDirectories;
 
-            var fileListQuery = Directory
-               .EnumerateFiles(directory.ID, searchPattern, searchOption)
-               .Where(file => !string.IsNullOrEmpty(file))
-               .Where(file => File.Exists(file))
-               .OrderBy(file => file)
-               .Take(limit)
-               .AsQueryable();
-            var fileList = await Task.FromResult(fileListQuery.ToArray());
+         var fileListQuery = Directory
+            .EnumerateFiles(directory.ID, searchPattern, searchOption)
+            .Where(file => !string.IsNullOrEmpty(file))
+            .Where(file => File.Exists(file))
+            .OrderBy(file => file)
+            .Take(limit)
+            .AsQueryable();
+         var fileList = await Task.FromResult(fileListQuery.ToArray());
 
-            var fileTasks = fileList
-               .Select(file => GetDetails(file))
-               .ToArray();
-            var fileTasksResult = await Task.WhenAll(fileTasks);
-            var fileResult = fileTasksResult
-               .Where(fileVM => fileVM != null)
-               .ToArray();
+         var fileTasks = fileList
+            .Select(file => GetDetails(file))
+            .ToArray();
+         var fileTasksResult = await Task.WhenAll(fileTasks);
+         var fileResult = fileTasksResult
+            .Where(fileVM => fileVM != null)
+            .ToArray();
 
-            return fileResult;
-         }
-         catch (Exception) { throw; }
+         return fileResult;
       }
 
    }
