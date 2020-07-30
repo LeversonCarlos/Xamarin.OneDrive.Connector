b02648983a062c85299592e42c22f1a54162bcfc
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-30 17:26:33 -0300

    this was a try that a will have to revert

--- tests/CloudDrive.Connector.OneDriveTests/Helpers/IdentityBuilder.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Helpers/IdentityBuilder.cs
@@ -1,15 +1,64 @@
 using Microsoft.Identity.Client;
 using Moq;
+using System;
 
 namespace Xamarin.CloudDrive.Connector.OneDriveTests
 {
+   internal interface IIdentity : IClientApplicationBase, IConfidentialClientApplication, IPublicClientApplication { }
+
    internal class IdentityBuilder
    {
-
-      readonly Mock<IClientApplicationBase> Mock;
-      public IdentityBuilder() => this.Mock = new Mock<IClientApplicationBase>();
+      readonly Mock<IIdentity> Mock;
+      public IdentityBuilder() => this.Mock = new Mock<IIdentity>();
       public static IdentityBuilder Create() => new IdentityBuilder();
-      public IClientApplicationBase Builder() => this.Mock.Object;
 
+      public IdentityBuilder WithTokenForClient(string accessCode, DateTimeOffset expiresOn, string[] scopes)
+      {
+         var result = IdentityResultForClientBuilder.Create().With(accessCode, expiresOn, scopes).Build();
+         this.Mock.Setup(m => m.AcquireTokenForClient(scopes)).Returns(result);
+         return this;
+      }
+
+      public IdentityBuilder WithTokenInteractive(string accessCode, DateTimeOffset expiresOn, string[] scopes)
+      {
+         var result = IdentityResultInteractiveBuilder.Create().With(accessCode, expiresOn, scopes).Build();
+         this.Mock.Setup(m => m.AcquireTokenInteractive(scopes)).Returns(result);
+         return this;
+      }
+
+      public IIdentity Build() => this.Mock.Object;
    }
+
+   internal class IdentityResultForClientBuilder
+   {
+      readonly Mock<AcquireTokenForClientParameterBuilder> Mock;
+      public IdentityResultForClientBuilder() => this.Mock = new Mock<AcquireTokenForClientParameterBuilder>();
+      public static IdentityResultForClientBuilder Create() => new IdentityResultForClientBuilder();
+
+      public IdentityResultForClientBuilder With(string accessCode, DateTimeOffset expiresOn, string[] scopes)
+      {
+         var result = new AuthenticationResult(accessCode, false, null, expiresOn, expiresOn.AddMinutes(1), null, null, null, scopes, Guid.Empty);
+         this.Mock.Setup(m => m.ExecuteAsync()).ReturnsAsync(result);
+         return this;
+      }
+
+      public AcquireTokenForClientParameterBuilder Build() => this.Mock.Object;
+   }
+
+   internal class IdentityResultInteractiveBuilder
+   {
+      readonly Mock<AcquireTokenInteractiveParameterBuilder> Mock;
+      public IdentityResultInteractiveBuilder() => this.Mock = new Mock<AcquireTokenInteractiveParameterBuilder>();
+      public static IdentityResultInteractiveBuilder Create() => new IdentityResultInteractiveBuilder();
+
+      public IdentityResultInteractiveBuilder With(string accessCode, DateTimeOffset expiresOn, string[] scopes)
+      {
+         var result = new AuthenticationResult(accessCode, false, null, expiresOn, expiresOn.AddMinutes(1), null, null, null, scopes, Guid.Empty);
+         this.Mock.Setup(m => m.ExecuteAsync()).ReturnsAsync(result);
+         return this;
+      }
+
+      public AcquireTokenInteractiveParameterBuilder Build() => this.Mock.Object;
+   }
+
 }
--- tests/CloudDrive.Connector.OneDriveTests/Tests/TokenTests.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Tests/TokenTests.cs
@@ -7,16 +7,32 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
    {
 
       [Fact]
-      public void ConstructorArgumentsMustBeSet()
+      public void Constructor_NullIdentityParameter_MustThrowException()
       {
-         var ex = Assert.Throws<ArgumentException>(() => new OneDriveToken(null, null));
-         Assert.Equal("The identity argument for the token client must be set", ex.Message);
+         var exception = Assert.Throws<ArgumentException>(() => new OneDriveToken(null, new string[] { "[test]" }));
+
+         Assert.NotNull(exception);
+         Assert.Equal("The identity argument for the token client must be set", exception.Message);
+      }
+
+      [Theory]
+      [InlineData(null, "The scopes argument for the onedrive client must be set")]
+      [InlineData(new string[] { }, "The scopes argument for the onedrive client must be set")]
+      [InlineData(new string[] { "" }, "The scopes argument for the onedrive client must be set")]
+      public void Constructor_InvalidScopesParameter_MustThrowException(string[] scopes, string message)
+      {
+         var identity = IdentityBuilder.Create().WithTokenForClient(null, DateTimeOffset.Now, null).Build();
+
+         var exception = Assert.Throws<ArgumentException>(() => new OneDriveToken(identity, scopes));
+
+         Assert.NotNull(exception);
+         Assert.Equal(message, exception.Message);
       }
 
       [Fact]
       public void InitialTokenMustBeEmpty()
       {
-         var identity = IdentityBuilder.Create().Builder();
+         var identity = IdentityBuilder.Create().Build();
          var token = new OneDriveToken(identity, null);
 
          var expected = "";
