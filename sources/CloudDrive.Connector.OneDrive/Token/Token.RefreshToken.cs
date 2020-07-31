f443c5e17317bd3cb56cf81032b4a2a114178680
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-30 21:38:54 -0300

    refactor the refrsh token function

--- sources/CloudDrive.Connector.OneDrive/Token/Token.RefreshToken.cs
+++ sources/CloudDrive.Connector.OneDrive/Token/Token.RefreshToken.cs
@@ -12,12 +12,9 @@ namespace Xamarin.CloudDrive.Connector
          try
          {
             var accounts = await Identity.GetAccountsAsync();
-            if (accounts?.Count() != 0)
-            {
-               var account = accounts.FirstOrDefault();
-               if (account != null)
-                  _AuthResult = await Identity.AcquireTokenSilentAsync(account);
-            }
+            var account = accounts?.FirstOrDefault();
+            if (account != null)
+               _AuthResult = await Identity.AcquireTokenSilentAsync(account);
             return this.IsTokenValid();
          }
          catch (Exception) { return false; }
