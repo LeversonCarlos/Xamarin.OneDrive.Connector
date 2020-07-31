02c08f171cd784ece6eeb6ae5d54e22804b1a7ee
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-31 16:51:04 -0300

    improuve this startup test

--- tests/CloudDrive.Connector.OneDriveTests/Tests/StartupTests.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Tests/StartupTests.cs
@@ -9,14 +9,18 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
       [Fact]
       public void AddOneDriveConnector_RegisteredInstance_MustNotBeNull()
       {
+         var clientID = "clientID";
+         var scopes = new string[] { "scopes" };
          var serviceCollection = new ServiceCollection();
          var serviceProvider = serviceCollection
-            .AddOneDriveConnector("clientID", "clientSecret", "redirectUri", new string[] { "scopes" })
+            .AddOneDriveConnector(clientID, "clientSecret", "redirectUri", scopes)
             .BuildServiceProvider();
 
          var value = serviceProvider.GetService<OneDriveSettings>();
 
          Assert.NotNull(value);
+         Assert.Equal(clientID, value.ClientID);
+         Assert.Equal(scopes, value.Scopes);
       }
 
    }
