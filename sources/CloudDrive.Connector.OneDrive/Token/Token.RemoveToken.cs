2280028bc27c217cad8927043aa27cc01492abce
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-31 15:17:04 -0300

    refactor the remove account function

--- sources/CloudDrive.Connector.OneDrive/Token/Token.RemoveToken.cs
+++ sources/CloudDrive.Connector.OneDrive/Token/Token.RemoveToken.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Xamarin.CloudDrive.Connector
@@ -7,15 +6,15 @@ namespace Xamarin.CloudDrive.Connector
    partial class OneDriveToken
    {
 
-      async Task RemoveTokenAsync()
+      internal async Task RemoveTokenAsync()
       {
          try
          {
-            var accounts = await this.Identity.GetAccountsAsync();
-            if (accounts != null && accounts.Count() != 0)
+            var accounts = await Identity.GetAccountsAsync();
+            if (accounts != null)
             {
                foreach (var account in accounts)
-               { await this.Identity.RemoveAsync(account); }
+                  await Identity.RemoveAsync(account);
             }
          }
          catch (Exception) { throw; }
