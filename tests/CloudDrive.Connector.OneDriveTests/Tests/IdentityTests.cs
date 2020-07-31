6b6cb445a35a913c16b71b3522b23768c7b932d8
Leverson Carlos <lcjohnny@hotmail.com>
2020-07-31 16:57:19 -0300

    another constructor test

--- tests/CloudDrive.Connector.OneDriveTests/Tests/IdentityTests.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Tests/IdentityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
 using System;
 using Xunit;
 
@@ -19,6 +20,22 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
 
          Assert.NotNull(value);
          Assert.Equal("The settings object for the onedrive client wasnt found on dependency service", value.Message);
+      }
+
+      [Fact]
+      public void Constructor_WithInvalidSettingsData_MustThrowException()
+      {
+         var clientID = "clientID";
+         var scopes = new string[] { "scopes" };
+         var serviceCollection = new ServiceCollection();
+         var serviceProvider = serviceCollection
+            .AddOneDriveConnector(clientID, "clientSecret", "redirectUri", scopes)
+            .BuildServiceProvider();
+
+         var value = Assert.Throws<MsalClientException>(() => serviceProvider.GetService<OneDriveIdentity>());
+
+         Assert.NotNull(value);
+         Assert.Equal("Error: ClientId is not a Guid.", value.Message);
       }
 
    }
