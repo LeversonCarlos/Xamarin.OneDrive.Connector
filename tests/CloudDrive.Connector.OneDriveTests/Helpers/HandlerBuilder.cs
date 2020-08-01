cbc9a9fc162e40beaf0ec371863af1dd35b05d3e
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-01 13:18:11 -0300

    improve this mocker to cretate a response message

--- tests/CloudDrive.Connector.OneDriveTests/Helpers/HandlerBuilder.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Helpers/HandlerBuilder.cs
@@ -17,15 +17,23 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
       ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SendAsync() =>
          Mock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
 
-      public HandlerBuilder WithGetAsync(HttpResponseMessage responseMessage)
-      {
-         SendAsync().ReturnsAsync(responseMessage);
-         return this;
-      }
       public HandlerBuilder WithGetAsync(Exception ex)
       {
          SendAsync().ThrowsAsync(ex);
          return this;
+      }
+      public HandlerBuilder WithGetAsync<T>(T value)
+      {
+         SendAsync().ReturnsAsync(GetResponseMessage(value));
+         return this;
+      }
+
+      HttpResponseMessage GetResponseMessage<T>(T value)
+      {
+         var jsonParameter = System.Text.Json.JsonSerializer.Serialize(value, new System.Text.Json.JsonSerializerOptions { });
+         var responseContent = new StringContent(jsonParameter, System.Text.Encoding.UTF8, "application/json");
+         var responseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = responseContent };
+         return responseMessage;
       }
 
       public HttpMessageHandler Build() => Mock.Object;
