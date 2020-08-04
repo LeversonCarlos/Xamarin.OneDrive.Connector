0e72d201edf86bdd32afed67fca7d47f76d871e6
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-04 19:15:07 -0300

    tests for the connection functions

---
+++ tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.Connection.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace Xamarin.CloudDrive.Connector.OneDriveTests
+{
+   partial class ServiceTests
+   {
+
+      [Fact]
+      public async void ConnectAsync_WithDefaultClient_MustResultFalse()
+      {
+         var client = ClientBuilder.Create().Build();
+         var service = new OneDriveService(client: client);
+
+         var value = await service.ConnectAsync();
+
+         Assert.False(value);
+      }
+
+      [Fact]
+      public async void CheckConnectionAsync_WithDefaultClient_MustResultFalse()
+      {
+         var client = ClientBuilder.Create().Build();
+         var service = new OneDriveService(client: client);
+
+         var value = await service.CheckConnectionAsync();
+
+         Assert.False(value);
+      }
+
+      [Fact]
+      public async void DisconnectAsync_WithDefaultClient_MustResultFalse()
+      {
+         var client = ClientBuilder.Create().Build();
+         var service = new OneDriveService(client: client);
+
+         var expected = false;
+         await service.DisconnectAsync();
+
+         Assert.False(expected);
+      }
+
+   }
+}
