50efdba3a4644731a2eaf9f792908ce9de2f3abf
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-04 21:02:08 -0300

    refactor the client builder helper

--- tests/CloudDrive.Connector.OneDriveTests/Helpers/ClientBuilder.cs
+++ tests/CloudDrive.Connector.OneDriveTests/Helpers/ClientBuilder.cs
@@ -1,27 +1,43 @@
 using Moq;
+using Moq.Language.Flow;
+using Moq.Protected;
 using System;
-using System.Linq.Expressions;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Xamarin.CloudDrive.Connector.OneDriveTests
 {
    internal class ClientBuilder
    {
-
-      readonly Mock<OneDriveClient> Mock;
-      public ClientBuilder() => this.Mock = new Mock<OneDriveClient>();
+      readonly Mock<HttpMessageHandler> Mock;
+      public ClientBuilder() => Mock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
       public static ClientBuilder Create() => new ClientBuilder();
 
-      public ClientBuilder With(Expression<Func<OneDriveClient, Task<HttpResponseMessage>>> expression, Exception ex)
+      ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SendAsync(HttpRequestMessage requestMessage) =>
+         Mock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync",
+            ItExpr.Is<HttpRequestMessage>(req => req.Method == requestMessage.Method && req.RequestUri == req.RequestUri),
+            ItExpr.IsAny<CancellationToken>());
+
+      public ClientBuilder With(HttpRequestMessage requestMessage, Exception ex)
       {
-         this.Mock.Setup(expression).ThrowsAsync(ex);
+         SendAsync(requestMessage).ThrowsAsync(ex);
          return this;
       }
-      public ClientBuilder With<R>(Expression<Func<OneDriveClient, Task<HttpResponseMessage>>> expression, R value)
+      public ClientBuilder With<T>(HttpRequestMessage requestMessage, T value)
       {
-         this.Mock.Setup(expression).ReturnsAsync(GetResponseMessage(value));
+         SendAsync(requestMessage).ReturnsAsync(GetResponseMessage(value));
          return this;
+      }
+
+      internal static HttpRequestMessage GetRequestMessage(string requestUri) =>
+         new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/" + requestUri);
+      internal static HttpRequestMessage GetRequestMessage<T>(string requestUri, T value, HttpMethod method)
+      {
+         var jsonContent = System.Text.Json.JsonSerializer.Serialize(value, new System.Text.Json.JsonSerializerOptions { });
+         var stringContent = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
+         var message = new HttpRequestMessage(method, requestUri) { Content = stringContent };
+         return message;
       }
 
       HttpResponseMessage GetResponseMessage<T>(T value)
@@ -32,6 +48,7 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
          return responseMessage;
       }
 
-      public OneDriveClient Build() => this.Mock.Object;
+      public OneDriveClient Build() =>
+         new OneDriveClient(TokenBuilder.Create().Build(), Mock.Object);
    }
 }
