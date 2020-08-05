7dae6e9041e567efc262f0e72ef505704f99eb91
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-04 21:22:58 -0300

    final test for the profile function

--- tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.Profile.cs
+++ tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.Profile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Xamarin.CloudDrive.Connector.OneDriveTests
@@ -28,6 +29,21 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
 
          Assert.NotNull(value);
          Assert.Equal(exception.Message, value.Message);
+      }
+
+      [Fact]
+      public async void GetProfile_WithSpecificData_MustResultSpectedValue()
+      {
+         var param = new DTOs.Profile { id = "id", displayName = "displayName", userPrincipalName = "userPrincipalName" };
+         var client = ClientBuilder.Create().With("me?$select=id,displayName,userPrincipalName", param).Build();
+         var service = new OneDriveService(client);
+
+         var value = await service.GetProfile();
+
+         Assert.NotNull(value);
+         Assert.Equal(value.ID, param.id);
+         Assert.Equal(value.Description, param.displayName);
+         Assert.Equal(value.KeyValues.Where(k => k.Key == "EMail").Select(v => v.Value).FirstOrDefault(), param.userPrincipalName);
       }
 
    }
