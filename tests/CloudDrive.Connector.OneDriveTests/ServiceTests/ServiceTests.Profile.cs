891d5249cd31a016a58aa108e36bb7a5e4953d63
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-05 12:15:19 -0300

    final test case for the profile picture function

--- tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.Profile.cs
+++ tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.Profile.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net.Http;
 using Xunit;
 
 namespace Xamarin.CloudDrive.Connector.OneDriveTests
@@ -41,9 +42,9 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
          var value = await service.GetProfile();
 
          Assert.NotNull(value);
-         Assert.Equal(value.ID, param.id);
-         Assert.Equal(value.Description, param.displayName);
-         Assert.Equal(value.KeyValues.Where(k => k.Key == "EMail").Select(v => v.Value).FirstOrDefault(), param.userPrincipalName);
+         Assert.Equal(param.id, value.ID);
+         Assert.Equal(param.displayName, value.Description);
+         Assert.Equal(param.userPrincipalName, value.KeyValues.Where(k => k.Key == "EMail").Select(v => v.Value).FirstOrDefault());
       }
 
       [Fact]
@@ -70,7 +71,20 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
          Assert.Equal(exception.Message, value.Message);
       }
 
+      [Fact]
+      public async void GetProfilePicture_WithSpecificData_MustResultSpectedValue()
+      {
+         var param = System.Text.Encoding.UTF8.GetBytes("Some Dummy Message");
+         var paramContent = new ByteArrayContent(param, 0, param.Length);
+         var paramMessage = new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = paramContent };
+         var client = ClientBuilder.Create().With("me/photo/$value", paramMessage).Build();
+         var service = new OneDriveService(client);
 
+         var value = await service.GetProfilePicture();
+
+         Assert.NotNull(value);
+         Assert.Equal(param, value);
+      }
 
    }
 }
