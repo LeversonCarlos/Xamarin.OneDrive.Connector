df34a3aa24faa7606500eb9dd07b71a2de0c284d
Leverson Carlos <lcjohnny@hotmail.com>
2020-08-06 19:21:32 -0300

    one more test for the search files function

--- tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.File.Search.cs
+++ tests/CloudDrive.Connector.OneDriveTests/ServiceTests/ServiceTests.File.Search.cs
@@ -14,10 +14,41 @@ namespace Xamarin.CloudDrive.Connector.OneDriveTests
          var service = new OneDriveService(client);
          var directory = new DirectoryVM { };
 
-         var value = await Assert.ThrowsAsync<ArgumentException>(async () => await service.SearchFiles(directory, new string[] { }, addFilesUntilLimit: null));
+         var value = await Assert.ThrowsAsync<ArgumentException>(async () => await service.SearchFiles(directory, "", 0));
 
          Assert.NotNull(value);
          Assert.Equal(exception.Message, value.Message);
+      }
+
+      [Fact]
+      internal async void SearchFiles_WithValidArguments_MustResultAsSpected()
+      {
+         var fileData = new DTOs.FileSearch
+         {
+            value = new DTOs.File[] {
+               new DTOs.File {
+                  file = new DTOs.FileDetails{ },
+                  id="fileID", name="fileName.txt",
+                  parentReference=new DTOs.DirectoryParent{ id="parentID", path="/parent/folderName" }
+               }
+            }
+         };
+         var directoryData = new DTOs.DirectorySearch
+         {
+            value = new DTOs.Directory[] { }
+         };
+         var client = ClientBuilder.Create()
+            .With("$select=id,name,createdDateTime,size,@microsoft.graph.downloadUrl,file,parentReference&$top=1000", fileData)
+            .With("$select=id,name,folder,parentReference&$top=1000", directoryData)
+            .Build();
+         var service = new OneDriveService(client);
+         var directory = new DirectoryVM { ID = "driveID!folderID" };
+
+         var value = await service.SearchFiles(directory, "*.txt");
+
+         Assert.NotNull(value);
+         Assert.NotEmpty(value);
+         Assert.Equal("fileID", value[0].ID);
       }
 
    }
